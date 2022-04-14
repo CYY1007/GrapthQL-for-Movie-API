ad2fd62c6dc367b55be0b07cfb0f3958b38a6b5d
yongwook <60510921+CYY1007@users.noreply.github.com>
2022-04-14 09:57:43 +0900

    Add files via upload
    Practice Mutation in GraphQL

--- db.js
+++ db.js
@@ -1,19 +1,60 @@
-export const People = [
-    {   id : 0, 
-        name:"nico",
-        age:18,
-        gender:"sda"},  
-     {  id: 1,
-         name: "dddasdfsdg", 
-         age:123,
-         gender:"adfds"},
-    {  id: 2,
-            name: "dgtgdsdg", 
-            age:134,
-            gender:"a4dds"},
- ]
+let movies = [
+    {
+        id:0,
+        name:"Star Wars",
+        score : 8,
+        
+    },
+    {
+        id:1,
+        name:"Avengers",
+        score : 7,
+    },
+    {
+        id:2,
+        name:"Star Wars",
+        score : 7,
+    },
+    {
+        id:3,
+        name:"Good Will hunting",
+        score : 8,
+    },
+    {
+        id:4,
+        name:"Harry Potter",
+        score : 8,
+    },
+    {
+        id:5,
+        name:"The King's man",
+        score : 8,
+    },
+]
+
+
+export const getMovies = () => movies;
 
  export const getById = id => {
-     const filteredPerson = People.filter(person => id === person.id);
+     const filteredMovie = movies.filter(movie => id === movie.id);
      return filteredPerson[0];
+ }
+
+ export const deleteMovie = (id) =>{
+     const cleanedMovies = movies.filter(movie => movie.id !== id)
+     if(movies > cleanedMovies){
+        movies = cleanedMovies
+        return true;
+    }else
+        false;
+ }
+
+ export const addMovie = (name, score) =>{
+     const newMovie = {
+        id:`${movies.length + 1}`, 
+        name,
+        score,
+     }
+     movies.push(newMovie);
+     return newMovie;
  }
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import { createServer } from "graphql-yoga";
 import typeDefs from "./graphql/typeDefs";
-import { getById, People } from "./db";
+import { addMovie, deleteMovie, getById, getMovies, People } from "./db";
 
 const server = createServer({
     schema:{
         typeDefs : typeDefs,
         resolvers : {
             Query:{
-                people : () => People,
-                person: (_,{id}) =>getById(id)
+                movies : () => getMovies(),
+                movie: (_,{id}) =>getById(id)
             },
+            Mutation:{
+                addMovie : (_,{name,score}) => addMovie(name, score),
+                deleteMovie : (_,{id}) => deleteMovie(id)
+            }
         }
     }
 })
